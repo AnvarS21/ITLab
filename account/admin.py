dd77c87e76c3fec72b14f5f0e1d4281ad107c286
AnvarS21 <shamsutdinovanvar21@gmail.com>
2024-04-06 07:06:12 +0600

    fixed admin for account links

--- account/admin.py
+++ account/admin.py
@@ -3,13 +3,20 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, SocialLinks
 
 
+class SocialLinksInline(admin.StackedInline):  # или TabularInline, в зависимости от предпочтений по отображению
+    model = SocialLinks
+    can_delete = False
+    verbose_name_plural = 'Социальные ссылки'
+
+
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'username', 'position', 'is_staff', 'get_social_links')
+    list_display = ('email', 'username', 'position', 'is_staff')
     list_filter = ('position', 'is_staff', 'is_superuser', 'is_active', 'groups')
     search_fields = ('email', 'username', 'position')
     ordering = ('email',)
     filter_horizontal = ()
+    inlines = [SocialLinksInline]  # Добавляем инлайн для социальных ссылок
 
     fieldsets = (
         (None, {'fields': ('email', 'username', 'password')}),
@@ -24,20 +31,6 @@ class CustomUserAdmin(UserAdmin):
             'fields': ('email', 'username', 'password1', 'password2'),
         }),
     )
-
-    def get_social_links(self, obj):
-        social_links = obj.social
-        return ", ".join(
-            [f"{field.verbose_name}: {getattr(social_links, field.name)}" for field in SocialLinks._meta.fields[1:]])
-
-    get_social_links.short_description = 'Соц. сети'
-
-
-@admin.register(SocialLinks)
-class SocialLinksAdmin(admin.ModelAdmin):
-    list_display = ('user', 'telegram', 'facebook', 'instagram', 'linkedin', 'github', 'site_cv', 'vk')
-    list_filter = ('user',)
-    search_fields = ('user__email', 'user__username')
 
 
 admin.site.unregister(CustomUser)
